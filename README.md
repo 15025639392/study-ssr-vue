80809b3b90f3ce31a6335f9a970af8f189cabaf1
ldy <danyang.liao@quanshi.com>
2021-06-13 01:16:15 +0800

    路由基本使用

---
+++ README.md
@@ -0,0 +1,7 @@
+## nuxt.js 路由--路由导航
+- a 表变迁
+  - 不推荐使用，会刷新整个页面
+- nuxt-link 组件
+  - https://router.vuejs.org/zh/api/#router-link-props
+- 编程式导航
+  - https://router.vuejs.org/zh/guide/essentials/navigation.html#%E7%BC%96%E7%A8%8B%E5%BC%8F%E7%9A%84%E5%AF%BC%E8%88%AA
---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    about
+    <h1> a 链接</h1>
+    <a href="/">首页</a>
+    <h2>router-link</h2>
+    <router-link to="/">首页</router-link>
+    <!---编程式导航-->
+    <button @click="onClick">编程导航</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    onClick(){
+      console.log('hello')
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
