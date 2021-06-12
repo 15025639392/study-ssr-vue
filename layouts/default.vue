e35299da4023182a0f5f3fa594b0496fa5baa794
ldy <danyang.liao@quanshi.com>
2021-06-13 02:15:02 +0800

    08-布局组件

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>layouts/default.vue 组件</h1>
+      <!-- 页面出口  相当于子路由 -->
+      <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'LayoutDefault'
+}
+</script>
+
+<style>
+
+</style>
