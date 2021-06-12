b9286403bfde1703934dcd9aec2ccb6f07c39358
ldy <danyang.liao@quanshi.com>
2021-06-13 02:57:30 +0800

    10-上下文对象

---
+++ pages/about/_id.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <h1>_id.vue</h1>
+      <div>{{article.test}}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:'IdAbout',
+    async asyncData(context){
+        const {data} = await axios({
+            method:'GET',
+            url:'http://localhost:3000/data.json'
+        })
+        
+        const find = data.posts.find(r=>r.id==context.params.id)
+        console.log(find)
+        debugger
+        return {
+            article:find
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
