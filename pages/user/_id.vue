992a8878623534eaf7cf96f64957028c120bcaa8
ldy <danyang.liao@quanshi.com>
2021-06-13 01:29:34 +0800

    动态路由

---
+++ pages/user/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>user page</h1>
+      <h3>{{$route.params.id}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'UserPage'
+}
+</script>
+
+<style>
+
+</style>
