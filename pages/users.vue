f34032f663c8a81cac3bad1ef6b7f53c059f84ac
ldy <danyang.liao@quanshi.com>
2021-06-13 01:40:06 +0800

    嵌套路由

---
+++ pages/users.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      Users 父路由
+      <nuxt-child/>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/users/foo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>foo 路由</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
